c1baec43411a2e7e376eab0c93fd6f31027cebb4
melina1401 <122402701+melina1401@users.noreply.github.com>
2024-03-01 16:15:01 +0330

    Update Footer.tsx
    Fix Buy MGC footer link

--- packages/uikit/src/components/Footer/Footer.tsx
+++ packages/uikit/src/components/Footer/Footer.tsx
@@ -91,7 +91,7 @@ const MenuItem: React.FC<FooterProps> = ({
             </Box>
             <Button
               as="a"
-              href="https://pancakeswap.finance/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
+              href="https://dex.rz.game/swap?outputCurrency=0xbb73BB2505AC4643d5C0a99c2A1F34B3DfD09D11"
               target="_blank"
               scale="sm"
               endIcon={<ArrowForwardIcon color={lightColors.backgroundAlt} />}
